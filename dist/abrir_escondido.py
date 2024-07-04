8de10f37b76508b35e49e6bc5e4be0c025c28eaa
Mateus Vilanova dos Passos <mate01vp@gmail.com>
2024-07-04 17:15:51 -0300

    adicionado script para abrir com terminal escondido

---
+++ dist/abrir_escondido.py
@@ -0,0 +1,10 @@
+import subprocess
+import os
+
+def startProgram():
+    SW_HIDE = 0
+    info = subprocess.STARTUPINFO()
+    info.dwFlags = subprocess.STARTF_USESHOWWINDOW
+    info.wShowWindow = SW_HIDE
+    subprocess.Popen(os.path.join(os.path.dirname(__file__) or '.','Canivete_0.1.exe'), startupinfo=info)
+startProgram()
