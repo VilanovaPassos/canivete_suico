1a31af45b4ba54a34657adcf92a59691360ffae4
Mateus Vilanova dos Passos <mate01vp@gmail.com>
2024-07-08 16:22:42 -0300

    melhorada interface de instalacao

--- dist/configurar.bat
+++ dist/configurar.bat
@@ -6,11 +6,11 @@ cd %~dp0
 @echo Copiando Canivete para dentro do Holyrics ...
 xcopy .\Canivete_0.1.exe C:\Holyrics\Holyrics\files
 
-@echo abrindo porta 80
+@echo abrindo porta 80 ...
 netsh advfirewall firewall add rule name="canivete_holyrics" protocol=TCP dir=in localport=80 action=allow
 netsh advfirewall firewall add rule name="canivete_holyrics" protocol=TCP dir=out localport=80 action=allow
 
-@echo Criando tarefa no agendador de tarefas
+@echo Criando tarefa no agendador de tarefas ...
 schtasks.exe /Create /ru SYSTEM /XML .\canivete_holyrics.xml /tn canivete
 
 schtasks /run /tn canivete
