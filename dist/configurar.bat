810a1f1507d23bf43fa4395c978676b87ed8856b
Mateus Vilanova dos Passos <mate01vp@gmail.com>
2024-07-08 16:19:36 -0300

    Adicionado comando para abrir porta em configurar.bat

--- dist/configurar.bat
+++ dist/configurar.bat
@@ -6,6 +6,14 @@ cd %~dp0
 @echo Copiando Canivete para dentro do Holyrics ...
 xcopy .\Canivete_0.1.exe C:\Holyrics\Holyrics\files
 
+@echo abrindo porta 80
+netsh advfirewall firewall add rule name="canivete_holyrics" protocol=TCP dir=in localport=80 action=allow
+netsh advfirewall firewall add rule name="canivete_holyrics" protocol=TCP dir=out localport=80 action=allow
+
 @echo Criando tarefa no agendador de tarefas
 schtasks.exe /Create /ru SYSTEM /XML .\canivete_holyrics.xml /tn canivete
+
+schtasks /run /tn canivete
+
+@echo instalacao concluida com sucesso!!
 pause
