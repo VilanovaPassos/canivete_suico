ae1a515e82c0dc069fc2a876a8a26bf1cad56f6e
Mateus Vilanova dos Passos <mate01vp@gmail.com>
2024-07-25 10:08:04 -0300

    adicionado conversor de PDF

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import gradio as gr
+import pypdfium2 as pdfium
 import os
 import socket
 import tempfile
@@ -7,6 +8,7 @@ import tempfile
 #definicoes caminhos
 HOLYRICS_VIDEO = "C:\\Holyrics\\Holyrics\\files\\media\\video" 
 HOLYRICS_AUDIO = "C:\\Holyrics\\Holyrics\\files\\media\\audio"
+HOLYRICS_IMAGEM = "C:\\Holyrics\\Holyrics\\files\\media\\image"
 TEMPORARIO = tempfile.gettempdir()
 #Pega indereço IP
 IP_ADDR = socket.gethostbyname(socket.gethostname())
@@ -34,7 +36,7 @@ function refresh() {
 }
 """
 
-
+# ************************ Youtube Downloader ************************
 
 def realiza_download_video(video_link, save_path, resolucao, nome="%(title)s"):
     #baixar video
@@ -70,7 +72,7 @@ def video_downloader(video_link, tipo, resolution, como_salvar, progress=gr.Prog
                 return "DOWNLOAD REALIZADO COM SUCESSO!!! ESCOLHA UMA PASTA PARA SALVARO O ARQUIVO", gr.DownloadButton(label="salvar", value=f"{TEMPORARIO}\\temp.mp4", visible=True)
         else:
             progress(0.65, desc="Iniciando Download...")
-            
+
             if como_salvar == "sim":
                 progress(0.8, desc="Baixando...")
                 realiza_download_audio(video_link, HOLYRICS_AUDIO)
@@ -85,7 +87,38 @@ def video_downloader(video_link, tipo, resolution, como_salvar, progress=gr.Prog
                 return "DOWNLOAD REALIZADO COM SUCESSO!!! ESCOLHA UMA PASTA PARA SALVARO O ARQUIVO", gr.DownloadButton(label="salvar", value=f"{TEMPORARIO}\\temp.mp3", visible=True)
     else:
         gr.Error("URL INVALIDA!!!")
+
+# ************************* Conversor PDF ****************************
+
+def converte(pdf, path, file_name):
+     # Load a document
+     pdf = pdfium.PdfDocument(pdf)
+     pdf.get_metadata_dict 
+
+     # Loop over pages and render
+     for i in range(len(pdf)):
+         page = pdf[i]
+         image = page.render(scale=4).to_pil()
+         image.save(f"{path}\\{file_name}.jpg")
+
             
+
+def pdf_converter(file, como_salvar):
+    # file name with extension
+    file_name = os.path.basename(file)
+
+    # file name without extension
+    file_name = os.path.splitext(file_name)[0]
+
+    if como_salvar != "sim":
+        converte(file, TEMPORARIO, file_name)
+
+        gr.Warning("Convertido com sucesso")
+        return gr.DownloadButton(label="salvar", value=f"{TEMPORARIO}\\{file_name}.jpg", visible=True)
+    else:
+        converte(file, HOLYRICS_IMAGEM, file_name)
+
+        gr.Warning("Convertido com sucesso")
 
 # ************************** PAGINAS *******************************
 
@@ -112,8 +145,19 @@ with gr.Blocks(css=css, title="Youtube Downloader", js=js_func) as demo:
         salvar_output = gr.DownloadButton("Salvar Como", visible=False)
 
         download_button.click(fn=video_downloader, inputs=[url_input, tipo_input, resolution_input, como_salvar_input], outputs=[progress_output, salvar_output])
-    with gr.Tab("Conversor"):
+
+    with gr.Tab("Conversor PDF"):
         gr.Markdown("CONVERSOR PDF ---> JPEG")
+
+        file_input = gr.File()
+
+        como_salvar_input = gr.Radio(label="Salvar arquivo direto no holyrics?", choices=["sim", "não"], value="sim")
+
+        salvar_output = gr.DownloadButton("Salvar Como", visible=False)
+
+        converte_btn = gr.Button("CONVERTER")
+
+        converte_btn.click(fn=pdf_converter, inputs=[file_input,como_salvar_input], outputs=salvar_output)
 
 if __name__ == "__main__":
     os.system(f'start http://{IP_ADDR}') #abre navegador 
